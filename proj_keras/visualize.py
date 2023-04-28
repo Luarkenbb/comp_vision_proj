0e1a0e23b1b4e84d2e1833bb1736d70ff143180a
josephymt <74708938+josephymt@users.noreply.github.com>
2023-04-28 15:48:18 +0800

    Added plot label images

--- proj_keras/visualize.py
+++ proj_keras/visualize.py
@@ -1,4 +1,3 @@
-# Step 1: Import necessary libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,11 +5,9 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 
-
-# Load the dataset from CSV files
 train_df = pd.read_csv("/Users/josephyim/Documents/COMP/COMP4423 Computer Vision/Project/train.csv")
 test_df = pd.read_csv("/Users/josephyim/Documents/COMP/COMP4423 Computer Vision/Project/test.csv")
-print("dataset loaded.")
+
 
 # Preprocess the dataset
 def preprocess(df):
@@ -21,7 +18,7 @@ def preprocess(df):
 
 train_images, train_labels = preprocess(train_df)
 test_images, test_labels = preprocess(test_df)
-print("dataset preprocessed.")
+#print("dataset preprocessed.")
 
 # Visualize example images
 label_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
@@ -36,4 +33,30 @@ def plot_images(images, labels, nrows=2, ncols=5):
         cv2.imwrite("vis_images/img_{}.png".format(i), images[i])
     plt.show()
 
-plot_images(train_images, train_labels)
+#plot_images(train_images, train_labels)
+
+# Plot n images of a specific label
+def plot_label_images(images, labels, label_index, nrows=2, ncols=5):
+    fig, axes = plt.subplots(nrows, ncols, figsize=(10, 4))
+    i = 0
+    for row in range(nrows):
+        for col in range(ncols):
+            while labels[i] != label_index:
+                i += 1
+            axes[row, col].imshow(images[i], cmap="gray")
+            axes[row, col].axis("off")
+            i += 1
+    plt.show()
+
+# # Plot 10 images of trousers
+# plot_label_images(train_images, train_labels, 1)
+
+# # Plot 10 images of coats
+# plot_label_images(train_images, train_labels, 4)
+
+# # Plot 10 images of sandals
+# plot_label_images(train_images, train_labels, 5)
+
+# Plot 10 images of each label
+for i in range(10):
+    plot_label_images(train_images, train_labels, i)
